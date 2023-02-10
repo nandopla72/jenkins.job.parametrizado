67b1d9a486d2c7e57dbfe213354289a8035909bb
nandopla72 <124599945+nandopla72@users.noreply.github.com>
2023-02-10 10:14:40 +0100

    Update parametrizadoDSL.groovy

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -1,9 +1,9 @@
 job('ejemplo2-job-DSL') {
 	description('Job DSL de ejemplo para el curso de Jenkins')
   	scm {
-      		git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
-        		node / gitConfigName('macloujulian')
-        		node / gitConfigEmail('macloujulian@gmail.com')
+      		git('https://github.com/nandopla72/jenkins.job.parametrizado.git', 'main') { node ->
+        		node / gitConfigName('nandopla72')
+        		node / gitConfigEmail('nandopla@yahoo.es')
       		}
     	} 
   	parameters {
